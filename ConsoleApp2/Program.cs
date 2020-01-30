bb82c67a83e4518525f0b80a684c6ff2c137435f
sandeep kr <sandeep kr>
2020-01-30 16:42:04 +0530

    added new project

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            {
+                int i, fact = 1, n;
+                Console.Write("Enter any Number: ");
+                n = int.Parse(Console.ReadLine());
+                for (i = 1; i <= n; i++)
+                {
+                    fact = fact * i;
+                }
+                Console.WriteLine("Factorial of " + n + " is: " + fact);
+                function(fact);
+
+            }
+            static void function(int fact)
+            {
+
+                if (fact % 2 == 0)
+                {
+                    Console.WriteLine("it is a even");
+                }
+                else
+                {
+                    Console.WriteLine("it i not even");
+                }
+            }
+            
+
+        }
+    }
+}
